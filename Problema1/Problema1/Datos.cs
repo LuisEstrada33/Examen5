a053baa0d53e90038e8d782f422fb713613d6738
LuisEstrada33 <43280434+LuisEstrada33@users.noreply.github.com>
2018-12-04 16:26:08 -0800

    Add files via upload

---
+++ Problema1/Problema1/Datos.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema1
+{
+    class Datos
+    {
+        public void AskValues()
+        {
+            
+            int elements;
+            int[] values;
+
+            Console.Write("Número de elementos: ");
+            elements = Convert.ToInt32(Console.ReadLine()); //Pide el número de elementos
+            
+                values = new int[elements]; //Inicializa el arreglo con los elementos indicados
+                Console.Clear();
+                Console.Write("Ingreso de datos: \n");
+
+            for (int counter = 0; counter < elements; counter++)
+            {
+               
+
+                while (counter < elements) // Simplemente se valida para que no te deje meter mas numeros que no sean 0,1,2 o letras
+
+                {
+                    Console.WriteLine("Elemento {0}: ", counter + 1);
+                    string temp = Console.ReadLine();
+
+                    int valor = 0;
+                    if (!int.TryParse(temp, out valor))
+                    {
+                        Console.WriteLine("El valor no es numerico.");
+                        continue;
+                    }
+
+                    List<int> opciones = new List<int>() { 0, 1, 2 };
+                    if (!opciones.Contains(valor))
+                    {
+                        Console.WriteLine("No es un valor permitido.");
+                        continue;
+                    }
+
+                    values[counter] = valor;
+                    counter++;
+
+                }
+
+            }
+            
+
+                
+
+                Bubble Order = new Bubble(); //Hace la instanciación a la clase Burbuja
+
+                Order.BubbleSort(values); //Se llama al método de bulbasaur a través del objeto
+            
+           
+        }
+    }
+}
---
+++ Problema1/Problema1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Datos Call = new Datos();//Se instancia la clase Datos
+
+            Call.AskValues(); //Se hace la llamada al método PedirValores
+        }
+    }
+}
